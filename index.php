a6d38b20ea79a1e4dd16fb06a8b3b85692381a2d
Jeff <github@314ghz.com>
2016-02-18 02:04:07 -0600

    Initial page.

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+	error_reporting(E_ALL);       # Report Errors, Warnings, and Notices
+	ini_set('display_errors', 1); # Display errors on page (instead of a log file)
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Jeff's xkcd Password Generator</title>
+		<link rel="stylesheet" type="text/css" href="xkcd.css">
+		<?php require 'logic.php'; ?>
+	</head>
+	<body>
+		<h1>xkcd Password Generator</h1>
+		<div><?php echo getXkcdRandomPassword($numWordsInput,$addNumberInput,$addSymbolInput) ?></div>
+		<br>
+		<form method='POST' action='index.php'>
+			Number of words: <input type="number" name="numWords" min="0" max="10" value="4">
+			<br>
+			<input type="checkbox" name="addNumber" value="1">Add a number
+			<br>
+			<input type="checkbox" name="addSymbol" value="1">Add a symbol
+			<br>
+			<input type="submit" value="Give me another">
+		</form>
+		<br>
+		<div class="error"></div>
+	</body>
+</html>
---
+++ logic.php
@@ -0,0 +1,78 @@
+<?php
+	$wordFile = fopen( "words.txt", "r" ) or die( "Unable to open words file!" );
+	$words = array();
+	while( ($word = fgets($wordFile, 4096)) !== false )
+	{
+		$words[] = trim($word);
+	}
+	fclose( $wordFile );
+
+	$error = "";
+
+	//Get the Form data
+	$numWordsInput = "4";
+	$addNumberInput = "0";
+	$addSymbolInput = "0";
+	if( array_key_exists('numWords', $_POST))
+	{
+		$numWordsInput = $_POST['numWords'];
+	}
+
+	if( array_key_exists('addNumber', $_POST))
+	{
+		$addNumberInput = $_POST['addNumber'];
+	}
+
+	if( array_key_exists('addSymbol', $_POST))
+	{
+		$addSymbolInput = $_POST['addSymbol'];
+	}
+	
+	function getXkcdRandomPassword( $numWords = '4', $addNumber = '0', $addSymbol = '0')
+	{
+		global $words;
+		global $error;
+		$symbols = array('!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '=');
+
+		echo "NumWords: " . $numWords . "<br>";
+		echo "addNumber: " . $addNumber . "<br>";
+		echo "addSymbol: " . $addSymbol . "<br>";
+
+		$password = "";
+		
+		if( is_numeric($numWords) === false )
+		{
+			$error = "Please enter a valid number for the number of words.";
+		}
+		else if( $numWords < 0 or $numWords > 10 )
+		{
+			$error = "Please enter a number between 0 and 10 for the number of words.";
+		}
+		else
+		{
+			for( $i = 0; $i < $numWords; $i++ )
+			{
+				if( $i == 0 )
+				{
+					$password = $password . $words[rand(0, count($words))];
+				}
+				else
+				{
+					$password = $password . '-' . $words[rand(0, count($words))];
+				}
+			}
+		}
+		
+		if( $addNumber !== "0" )
+		{
+			$password = $password . rand(0, 100);
+		}
+		
+		if( $addSymbol !== "0" )
+		{
+			$password = $password . $symbols[rand(0, count($symbols))];
+		}
+		
+		return $password;
+	}
+?>
